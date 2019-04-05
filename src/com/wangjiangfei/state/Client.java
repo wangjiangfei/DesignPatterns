d81e64b3e3617abd0cb5b1e22a68484dcb79dd19
wangjiangfei <2372315243@qq.com>
2019-04-05 09:09:50 +0800

    状态模式

---
+++ src/com/wangjiangfei/state/Client.java
@@ -0,0 +1,11 @@
+package com.wangjiangfei.state;
+
+public class Client {
+
+    public static void main(String[] args) {
+        VoteManager vm = new VoteManager();
+        for (int i = 0; i < 8; i++) {
+            vm.vote("u1", "A");
+        }
+    }
+}
