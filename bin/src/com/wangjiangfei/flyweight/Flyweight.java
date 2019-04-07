b1825d8905ef399f6e93d666b4cb122c3c2145ba
wangjiangfei <2372315243@qq.com>
2019-04-07 20:28:57 +0800

    享元模式

---
+++ bin/src/com/wangjiangfei/flyweight/Flyweight.java
@@ -0,0 +1,17 @@
+package com.wangjiangfei.flyweight;
+
+/**
+ * 描述授权数据的享元接口
+ *
+ * @author wangjiangfei
+ */
+public interface Flyweight {
+
+    /**
+     * 判断传入的安全实体和权限，是否和享元对象内部状态匹配
+     * @param securityEntity 安全实体
+     * @param permit 权限
+     * @return true表示匹配，false表示不匹配
+     */
+    boolean match(String securityEntity, String permit);
+}
